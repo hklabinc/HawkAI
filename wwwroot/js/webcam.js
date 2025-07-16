ac73ce4ef344f1192fe4e0e93ac702f108dda8a8
hopper80 <hhchoi80@gmail.com>
2025-07-16 17:10:38 +0900

    Update

--- wwwroot/js/webcam.js
+++ wwwroot/js/webcam.js
@@ -1,4 +1,18 @@
 ﻿/* Gobally 사용을 위하여 아래 window.를 붙임 */
+window.captureWebcamImage = async function () {
+    const video = document.createElement('video');
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream;
+    await video.play();
+
+    const canvas = document.createElement('canvas');
+    canvas.width = 640;
+    canvas.height = 480;
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    stream.getTracks().forEach(track => track.stop());
+
+    return canvas.toDataURL('image/jpeg');
+}
 
 /* WebCam Play */
 window.ShowVideo = () => {
